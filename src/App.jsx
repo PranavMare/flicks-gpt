18c7c30813f5f2e95f73187eda52f7169cf82c99
Pranav Mare <pranavmare99@gmail.com>
2025-09-01 16:36:36 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="">FlicksGPT - AI movie search by vibe, scene, or quote.</div>
+    </div>
+  );
+}
+
+export default App;
